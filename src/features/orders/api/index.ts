68e9c412abbda00e31d7ad1dd619cd552763d5d9
Vadzim Melnikau <v.melnikau@godeltech.com>
2021-10-05 17:10:08 +0300

    order status change send notification

--- src/features/orders/api/index.ts
+++ src/features/orders/api/index.ts
@@ -1,8 +1,20 @@
 import { customAlphabet } from "nanoid";
 import { TCheckOrderData, TOrder } from "features/orders/types";
 import { TCart } from "features/cart/types";
+import notification from "antd/lib/notification";
 
 const nanoid = customAlphabet("1234567890", 3);
+const sendNotification = ({ orderId, status }: TCheckOrderData) => {
+  const config = {
+    message: "Order status changed",
+    description: `Your order № ${orderId} changed status to ${status.toUpperCase()}.`,
+    duration: 5,
+  };
+  if (status !== "closed") {
+    return notification.info(config);
+  }
+  return notification.success(config);
+};
 
 /** FAKE API TO CREATE ORDER **/
 export const createOrderRequest = ({
@@ -38,6 +50,10 @@ export const checkOrderStatusRequest = ({
           orderId,
           status: "cooking",
         });
+        sendNotification({
+          orderId,
+          status: "cooking",
+        });
       }, 10000);
     });
   }
@@ -48,12 +64,20 @@ export const checkOrderStatusRequest = ({
           orderId,
           status: "ready",
         });
+        sendNotification({
+          orderId,
+          status: "ready",
+        });
       }, 10000);
     });
   }
   return new Promise((res) => {
     setTimeout(() => {
       res({
+        orderId,
+        status: "closed",
+      });
+      sendNotification({
         orderId,
         status: "closed",
       });
